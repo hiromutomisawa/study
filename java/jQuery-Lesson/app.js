039b8cc1c8c43ee19473645c73aa29b071d157a0
hiromutomisawa <peachcocoaht202101@gmail.com>
2021-07-26 19:53:01 +0900

    java

--- java/jQuery-Lesson/app.js
+++ java/jQuery-Lesson/app.js
@@ -5,13 +5,40 @@ $(function(){
       'width': '200px',
       'height': '100px'
     });
-  }).slideUp();
+  }).slideUp(1500);
 });
 // 解説
 // javascriptでは、引数の中に関数を入れることができるため
 // slideDownの引数にfunctionを書き加えることでスライドダウンした後に次のメソッドを実行できる。
 // 最初はすべて同じ階層に記述していたためアニメーションが干渉しうまく動作しなかった。
 // 補足 ---sideDown,Upの第一引数に数字を入力することで速度の変更可能(単位はm秒)
+
+
+// // マウスオーバー、アウトイベント
+// $(function(){
+//   $('.box2').mouseover(function(){
+//     $('.box2').css({
+//       'background-color': '#0000ff'
+//     });
+//   });
+//   $('.box2').mouseout(function(){
+//     $('.box2').css({
+//       'background-color': 'black'
+//     });
+//   });
+// });
+
+// 上記の方法だとCSSをいちいち記入する手間がかかるため
+// cssをあらかじめ定義しておき、class属性を追加(add)/解除(remove)するように設定する
+$(function(){
+  $('.box2').on('click',function() {
+    $('.box2').addClass('box2-ext');
+  });
+  $('.box2').mouseout(function() {
+    $('.box2').removeClass('box2-ext');
+  });
+});
+
 
 
 // $(function(){
